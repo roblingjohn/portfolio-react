cf8eab14bc94d0a657b8c4ae91e293bcccaa928e
John Robling <roblingjohn@gmail.com>
2020-08-05 20:57:41 -0400

    formatted navbar page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,39 +4,39 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-light">
-        {/* Brand */}
-        <Link className="navbar-brand" to="/">
-          John Robling
-        </Link>
-        {/* Toggler/collapsibe Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">
-                Portfolio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+      {/* Brand */}
+      <Link className="navbar-brand" to="/">
+        John Robling
+      </Link>
+      {/* Toggler/collapsibe Button */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      {/* Navbar links */}
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
