97b4962fb9af6804112052c9f5410935fb83e5d1
John Robling <roblingjohn@gmail.com>
2020-08-05 20:00:54 -0400

    set up react router

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import axios from "axios";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './containers/Home'
+import NoMatch from './containers/NoMatch/NoMatch';
+
+function App() {
+  useEffect(() => {
+    axios.get("/api/config")
+    .then((res) => {
+      console.log(res.data)
+    })
+  })
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route component={NoMatch}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
