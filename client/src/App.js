9cc7b384eb4f846aaef6fabb5347b8374e2b303b
John Robling <roblingjohn@gmail.com>
2020-08-05 20:26:26 -0400

    created container and component pages and linked components to home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react';
 import axios from "axios";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from './containers/Home/Home'
+import About from './containers/About/About'
+import Portfolio from './containers/Portfolio/Portfolio'
+import Contact from './containers/Contact/Contact'
 import NoMatch from './containers/NoMatch/NoMatch';
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/portfolio" component={Portfolio}/>
+        <Route exact path="/contact" component={Contact}/>
         <Route component={NoMatch}/>
       </Switch>
     </Router>
---
+++ client/src/containers/Portfolio/Portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the portfolio page.</h1>
+            </div>
+        );
+    }
+}
+
+export default Portfolio;
