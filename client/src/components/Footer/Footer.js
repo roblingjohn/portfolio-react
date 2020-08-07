8002274b685296043de113f60277b57a6f43a86a
John Robling <roblingjohn@gmail.com>
2020-08-06 21:39:49 -0400

    added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer id="sticky-footer" className="text-center">
+      <h6>Copyright 2020 John Robling</h6>
+    </footer>
+  );
+};
+
+export default Footer;
