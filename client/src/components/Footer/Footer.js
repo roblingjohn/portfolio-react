c49991c9788958f1ed637348d0c55837f1bf6467
John Robling <roblingjohn@gmail.com>
2021-02-16 14:34:10 -0500

    added 2021 to copyright year

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer id="sticky-footer" className="text-center">
-      <h6>Copyright 2020 John Robling</h6>
+      <h6>Copyright 2020-2021 John Robling</h6>
     </footer>
   );
 };
